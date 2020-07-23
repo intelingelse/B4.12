192d2d3a9522b8061cc7db8b2e36416069864073
Ashim Bayashev <intelingelse@gmail.com>
2020-07-24 01:13:24 +0600

    Add files via upload

---
+++ homework/find_athlete.py
@@ -0,0 +1,132 @@
+# ЗАДАНИЕ 2
+# Напишите модуль find_athlete.py поиска ближайшего к пользователю атлета. Логика работы модуля такова:
+#
+# 1) запросить идентификатор пользователя;
+# 2) если пользователь с таким идентификатором существует в таблице user, то вывести на
+#   экран двух атлетов: ближайшего по дате рождения к данному пользователю и ближайшего
+#   по росту к данному пользователю;
+# 3) если пользователя с таким идентификатором нет, вывести соответствующее сообщение.
+
+
+import sqlalchemy as sql
+from sqlalchemy import text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DB_PATH = "sqlite:///sochi_athletes.sqlite3"
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    Описывает структуру таблицы user для хранения данных пользователей
+    """
+    __tablename__ = "user"
+    id = sql.Column(sql.INTEGER, primary_key=True, autoincrement=True)
+    first_name = sql.Column(sql.TEXT)
+    last_name = sql.Column(sql.TEXT)
+    gender = sql.Column(sql.TEXT)
+    email = sql.Column(sql.TEXT)
+    birthdate = sql.Column(sql.TEXT)
+    height = sql.Column(sql.FLOAT)
+
+
+class Athlete(Base):
+    __tablename__ = "athelete"
+    id = sql.Column(sql.INTEGER, primary_key=True, autoincrement=True)
+    age = sql.Column(sql.INTEGER)
+    birthdate = sql.Column(sql.TEXT)
+    gender = sql.Column(sql.TEXT)
+    height = sql.Column(sql.FLOAT)
+    name = sql.Column(sql.TEXT)
+    weight = sql.Column(sql.INTEGER)
+    gold_medals = sql.Column(sql.INTEGER)
+    silver_medals = sql.Column(sql.INTEGER)
+    bronze_medals = sql.Column(sql.INTEGER)
+    total_medals = sql.Column(sql.INTEGER)
+    sport = sql.Column(sql.TEXT)
+    country = sql.Column(sql.TEXT)
+
+
+def find_closest_by_dob(dob_string, session):
+    """
+    Производит поиск по атлетам с ближайшим днем рождения к пользовательскому
+    Возвращает объект класса 'sqlalchemy.util._collections.result' в виде кортежа
+    :param dob_string: User date of_birth
+    :param session: session obj
+    :return: <class 'sqlalchemy.util._collections.result'> with (Athlete.name, Athlete.birthdate)
+    """
+
+    found_athlete = session.query(Athlete.name, Athlete.birthdate).order_by(
+        text("abs(strftime('%s', '{}') - strftime('%s', birthdate))".format(dob_string))).limit(1);
+
+    found_athlete = found_athlete[0]
+
+    return found_athlete
+
+
+def find_closest_by_height(height, session):
+    """
+    Производит поиск по атлетам с ближайшим ростом к пользовательскому
+    Возвращает объект класса 'sqlalchemy.util._collections.result' в виде кортежа
+    :param height: User height
+    :param session: session obj
+    :return:
+    """
+    found_athlete = session.query(Athlete.name, Athlete.height) \
+        .filter(Athlete.height != 0) \
+        .order_by(text("abs(height-%f)" % height)) \
+        .limit(1)
+
+    found_athlete = found_athlete[0]
+
+    return found_athlete
+
+
+def connect_db():
+    """
+    Устанавливает соединение к базе данных, создает таблицы, если их еще нет и возвращает объект сессии
+    :return: session object
+    """
+    engine = sql.create_engine(DB_PATH)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(engine)
+    return session()
+
+
+def main():
+    find_session = connect_db()
+
+    uid = input("Введите ID пользователя:\n>>>>  ")
+
+    user_query = find_session.query(User).filter(uid == User.id).first()
+    try:
+        while not user_query:
+            print("Пользователь с таким ID не найден.")
+            uid = input("Введите ID существующего пользователя или нажмите CTRL+D, чтобы выйти:\n>>>>  ")
+            user_query = find_session.query(User.first_name, User.last_name, User.birthdate, User.height).filter(
+                uid == User.id).first()
+    except EOFError as e:
+        print("exiting...")
+        exit()
+
+    user_full_name = user_query.first_name + " " + user_query.last_name
+    height = user_query.height
+    dob_string = user_query.birthdate
+    del user_query
+
+    close_height_athlete = find_closest_by_height(height=height, session=find_session)
+    close_birth_athlete = find_closest_by_dob(dob_string=dob_string, session=find_session)
+    print(
+        f"Атлета с ближайшим ростом к пользователю {user_full_name} зовут {close_height_athlete.name}. "
+        f"Его/Её рост составляет {close_height_athlete.height} метра")
+
+    print(
+        f"Атлета с ближайшей датой рождения к пользователю {user_full_name} зовут {close_birth_athlete.name}. "
+        f"Его/Её дата рождения: {close_birth_athlete.birthdate} ")
+
+    find_session.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ homework/users.py
@@ -0,0 +1,144 @@
+# ЗАДАНИЕ 1
+# Напишите модуль users.py, который регистрирует новых пользователей. Скрипт должен запрашивать следующие данные:
+#
+#     -имя
+#     -фамилию
+#     -пол
+#     -адрес электронной почты
+#     -дату рождения
+#     -рост
+#
+# Все данные о пользователях сохраните в таблице user нашей базы данных sochi_athletes.sqlite3.
+
+
+
+
+import datetime
+import sqlalchemy as sql
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DB_PATH = "sqlite:///sochi_athletes.sqlite3"
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    Описывает структуру таблицы user для хранения регистрационных данных пользователей
+    """
+    __tablename__ = "user"
+    id = sql.Column(sql.INTEGER, primary_key=True, autoincrement=True)
+    first_name = sql.Column(sql.TEXT)
+    last_name = sql.Column(sql.TEXT)
+    gender = sql.Column(sql.TEXT)
+    email = sql.Column(sql.TEXT)
+    birthdate = sql.Column(sql.TEXT)
+    height = sql.Column(sql.FLOAT)
+
+
+def connect_db():
+    """
+    Устанавливает соединение к базе данных, создает таблицы, если их еще нет и возвращает объект сессии
+    """
+    engine = sql.create_engine(DB_PATH)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(engine)
+
+    return session()
+
+
+def valid_email(email_string):
+    """
+    validates "email" string
+    :param email_string
+    :return: True or False
+    """
+    if "@" and "." in email_string:
+        if email_string.rfind("@") > email_string.rfind(".") or email_string.count("@") > 1 \
+                or not email_string.count("@") or not email_string.count("."):
+            return False
+        else:
+            return True
+
+
+def valid_dob(dob_string):
+    """
+    validates "date of birth" string
+    :param dob_string: date of birth
+    :return: True or False
+    """
+    now = datetime.datetime.now()
+    if len(dob_string) != 10 \
+            or dob_string.count("-") < 2 \
+            or int(dob_string[0:4]) < now.year - 123 \
+            or int(dob_string[5:7]) > 12 \
+            or int(dob_string[8:10]) > 31:
+        return False
+    else:
+        return True
+
+
+def valid_gender(gender_string):
+    """
+    validates "gender" string
+    :param gender_string:
+    :return: true or false
+    """
+    return True if gender_string == "Female" or gender_string == "Male" else False
+
+
+def request_data():
+    """
+    Запрашивает у пользователя данные и добавляет их в список users
+    :return: returns user object
+    """
+    # выводим приветствие
+    print("Пожалуйста, введите данные\n")
+    first_name = input("Ваше имя:\n>>>>  ")
+    last_name = input("Ваша фамилия:\n>>>>  ")
+    gender = input("Ваш пол (Male/Female)\n>>>>  ").capitalize()
+
+    while not valid_gender(gender):
+        print("Пол введен неверно!\n")
+
+        gender = input("Введите Ваш пол (Male/Female)\n>>>>  ")
+
+    email = input("Адрес Вашей электронной почты:\n>>>>  ")
+
+    while not valid_email(email):
+        print("Адрес почты введен неверно!\n")
+
+        email = input("Введите Адрес Вашей электронной почты:\n>>>>  ")
+
+    birthdate = input("Введите Вашу дату рождения в формате: YYYY-mm-dd:\n>>>>  ")
+
+    while not valid_dob(birthdate):
+        print("Дата рождения указана неверно!\n")
+
+        birthdate = input("Введите Вашу дату рождения в формате: YYYY-mm-dd:\n>>>>  ")
+
+    height = float(input("Введите свой рост (в сантиметрах):\n>>>>  "))
+    height /= 100
+
+    user = User(
+        first_name=first_name,
+        last_name=last_name,
+        gender=gender,
+        email=email,
+        birthdate=birthdate,
+        height=height
+    )
+    return user
+
+
+def main():
+    reg_session = connect_db()
+    new_user = request_data()
+    reg_session.add(new_user)
+    reg_session.commit()
+    print("Данные приняты!")
+
+
+if __name__ == '__main__':
+    main()
